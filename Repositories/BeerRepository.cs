fd5fd3003e51a038fc52fae6917dc9e6830d2fcd
Arthur d'Hooge <arthur.dhooge@outlook.com>
2021-04-24 11:35:26 +0200

    Added Beer repo

--- Repositories/BeerRepository.cs
+++ Repositories/BeerRepository.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace Project_BackDev.Repositories
+using Microsoft.EntityFrameworkCore;
+
+using BeerApi.DataContext;
+using BeerApi.Models;
+
+namespace BeerApi.Repositories
 {
-    public class BeerRepository
+    public interface IBeerRepository
     {
-        
+        Task<Beer> AddBeer(Beer beer);
+        Task<Beer> GetBeer(int beerId);
+        Task<List<Beer>> GetBeers();
+    }
+
+    public class BeerRepository : IBeerRepository
+    {
+        private IBeerContext _context;
+
+        public BeerRepository(IBeerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Beer>> GetBeers()
+        {
+            return await _context.Beers.ToListAsync();
+        }
+
+        public async Task<Beer> GetBeer(int beerId)
+        {
+            return await _context.Beers.Where(b => b.BeerId == beerId).SingleOrDefaultAsync();
+        }
+
+        public async Task<Beer> AddBeer(Beer beer)
+        {
+            await _context.Beers.AddAsync(beer);
+            await _context.SaveChangesAsync();
+            return beer;
+        }
     }
 }
